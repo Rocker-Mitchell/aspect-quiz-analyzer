701fd933043c79554646dc1138d4451429dfab69
Rocker-Mitchell <rocker.mat94@gmail.com>
2022-03-20 08:20:52 -0600

    upgrade to tailwindcss 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 module.exports = {
-  purge: ['./src/**/*.html', './src/**/*.js', './src/**/*.ts'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./src/**/*.{html,js,ts}'],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],

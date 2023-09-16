03fda2c56bc4304e048f961e49126c6c34485645
Rocker-Mitchell <rocker.mat94@gmail.com>
2023-09-16 08:07:24 -0600

    Update text styles and element use

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,9 @@
 
 <Section>
 	<header class="container mx-auto px-2 py-4 text-center">
-		<h1 class="font-saira text-4xl font-bold text-neutral-800 font-stretch-semi-expanded">
+		<h1
+			class="font-saira text-4xl font-bold tracking-tight text-neutral-800 font-stretch-semi-expanded"
+		>
 			Aspect Quiz Analyzer
 		</h1>
 		<p class="font-saira text-lg font-medium opacity-80 font-stretch-semi-expanded">
@@ -14,7 +16,7 @@
 	</header>
 
 	<div class="container mx-auto space-y-4 bg-neutral-200 px-4 py-8 text-center">
-		<p class="text-2xl font-medium font-stretch-expanded">Take the quiz here!</p>
+		<h2 class="text-2xl font-stretch-semi-expanded">Take the quiz here!</h2>
 
 		<ButtonLink href="/quiz">Begin</ButtonLink>
 	</div>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -4,7 +4,9 @@
 
 <header class="bg-neutral-200">
 	<div class="container mx-auto flex items-center gap-4 px-2 py-4">
-		<h1 class="p-0.5 font-saira text-2xl font-bold leading-7 font-stretch-semi-expanded">
+		<h1
+			class="p-0.5 font-saira text-2xl font-bold leading-7 tracking-tight font-stretch-semi-expanded"
+		>
 			<a href="/" class="cursor-pointer">AQA</a>
 		</h1>
 

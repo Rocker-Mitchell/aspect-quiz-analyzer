d1a0817f8bd3b429d023e87d051ace099af658d2
Rocker-Mitchell <rocker.mat94@gmail.com>
2023-09-16 07:56:15 -0600

    Remove button link's unused class prop

--- src/lib/ui/ButtonLink.svelte
+++ src/lib/ui/ButtonLink.svelte
@@ -1,13 +1,9 @@
 <script lang="ts">
 	export let href: string;
-	let addClass: string | undefined = undefined;
-	export { addClass as class };
-
-	$: appendClass = addClass || '';
 </script>
 
 <a
 	{href}
-	class="inline-block cursor-pointer rounded-xl bg-neutral-600 px-5 py-2 font-saira text-xl font-semibold uppercase leading-none text-neutral-50 shadow active:translate-y-px active:shadow-sm active:brightness-95 {appendClass}"
+	class="inline-block cursor-pointer rounded-xl bg-neutral-600 px-5 py-2 font-saira text-xl font-semibold uppercase leading-none text-neutral-50 shadow active:translate-y-px active:shadow-sm active:brightness-95"
 	><slot /></a
 >

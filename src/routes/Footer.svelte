ec463d786919263e32309fec089874eac045c384
Mitchell <rocker.mat94@gmail.com>
2023-10-16 11:30:18 -0600

    Update site footer
    Add link to GitHub repo
    
    Tweak spacing

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -3,12 +3,19 @@
 </script>
 
 <footer class="bg-neutral-200">
-	<div class="container mx-auto px-2 py-5">
+	<div class="container mx-auto space-y-3 px-2 py-9">
+		<p class="text-center text-sm font-medium opacity-80">
+			Source on <Link href="https://github.com/Rocker-Mitchell/aspect-quiz-analyzer" target="_blank"
+				>GitHub</Link
+			>
+		</p>
+
 		<p class="text-center text-sm font-medium opacity-80">
 			<!-- use inline blocks to have line breaks between spans -->
 			<Link href="http://hs.hiveswap.com/ezodiac/index.php" target="_blank" inlineBlock
 				>The Extended Zodiac</Link
-			> <span class="inline-block">owned by</span>
+			>
+			<span class="inline-block">owned by</span>
 			<span class="inline-block">What Pumpkin Games Inc.</span>
 		</p>
 	</div>

e5f7a9bd6638eba664ea4682a4dca04db6717779
Mitchell <rocker.mat94@gmail.com>
2024-01-15 10:29:06 -0700

    Configure pages to prerender

--- src/routes/quiz/+page.ts
+++ src/routes/quiz/+page.ts
@@ -1,6 +1,8 @@
 import type { PageLoad } from './$types';
 import type { HeadData } from '$lib/head-data/head-data';
 
+export const prerender = true;
+
 export const load: PageLoad = () => {
 	return {
 		title: 'Take the Quiz',

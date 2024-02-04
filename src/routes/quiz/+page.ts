133f34c728eb4fe42cbd43a2bda99d2843c170f7
Mitchell <rocker.mat94@gmail.com>
2024-02-04 10:36:24 -0700

    Fix quiz page prerender error
    Pages with actions can't be prerendered because the server must handle POST requests separately from GET

--- src/routes/quiz/+page.ts
+++ src/routes/quiz/+page.ts
@@ -1,8 +1,6 @@
 import type { PageLoad } from './$types';
 import type { HeadData } from '$lib/head-data/head-data';
 
-export const prerender = true;
-
 export const load: PageLoad = () => {
 	return {
 		title: 'Take the Quiz',

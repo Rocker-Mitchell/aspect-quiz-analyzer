95e3e9e25d5be2d0e0d561e4f493a67b06b0edfb
Rocker-Mitchell <rocker.mat94@gmail.com>
2024-04-09 11:00:08 -0600

    Update home card
    Remove heading as it's redundant to header's logo-title
    
    Add responsive behavior to have smaller text/less line wrapping on small screens

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,18 +5,18 @@
 <section
 	class="relative flex h-full flex-col justify-center px-2 py-12 before:absolute before:inset-0 before:-z-10 before:bg-[url(aspect-wheel-bg.svg)] before:bg-cover before:bg-center before:opacity-75"
 >
-	<div class="mx-auto w-fit space-y-4 rounded-2xl bg-neutral-50 px-3 py-6 shadow-xl">
-		<h1
-			class="text-center font-saira text-4xl font-bold tracking-tight text-neutral-800 font-stretch-semi-expanded"
-		>
-			Aspect Quiz Analyzer
-		</h1>
-
-		<p class="mx-auto max-w-prose text-pretty text-center">
-			Detailed score analysis of the <span class="inline-block">Extended Zodiac's</span> Aspect quiz
+	<div
+		class="mx-auto max-w-sm space-y-4 rounded-2xl bg-neutral-50 px-3.5 py-6 text-center shadow-xl sm:max-w-lg"
+	>
+		<p class="text-balance text-xl font-stretch-semi-expanded sm:text-2xl">
+			Detailed score analysis of the Extended Zodiac's Aspect quiz
 		</p>
 
-		<div class="text-center">
+		<p class="text-balance text-xl font-stretch-semi-expanded sm:text-2xl">
+			Take the quiz and see your results!
+		</p>
+
+		<div>
 			<BigButton type="anchor" href="/quiz">Begin</BigButton>
 		</div>
 	</div>

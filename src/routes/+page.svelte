e6c2ce2b7e93c0480ab3f0a8ae32658fd39c1b7b
Rocker-Mitchell <rocker.mat94@gmail.com>
2024-05-13 16:24:33 -0600

    Update background file path
    Build flow was reporting failure to resolve the background SVG file at build time, which made no background show on build preview. Add relative path so Vite can resolve the built path.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <section
-	class="relative flex h-full flex-col justify-center px-2 py-12 before:absolute before:inset-0 before:-z-10 before:bg-[url(aspect-wheel-bg.svg)] before:bg-cover before:bg-center before:opacity-75"
+	class="relative flex h-full flex-col justify-center px-2 py-12 before:absolute before:inset-0 before:-z-10 before:bg-[url('/aspect-wheel-bg.svg')] before:bg-cover before:bg-center before:opacity-75"
 >
 	<div
 		class="mx-auto max-w-sm space-y-4 rounded-2xl bg-neutral-50 px-3.5 py-6 text-center shadow-xl sm:max-w-lg"
